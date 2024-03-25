16575ae8686a3c1c77f4e3ca33fc5e876001564a
Tom Gardner <thomas.gardner@lntsoftware.com>
2024-03-25 17:11:47 +0000

    Update windows-service.js

--- windows-service.js
+++ windows-service.js
@@ -1,10 +1,30 @@
 const Service = require('node-windows').Service;
+
+const password = process.argv[2];
+if (!password) {
+  console.error('Usage: node windows-service.js <password>');
+  process.exit(1);
+}
 const svc = new Service({
   name: 'Toms Pc manager',
   description: 'Node.js app to manage a pc via the browser',
-  script: 'app.js'
+  script: 'app.js',
+  username: '',
+  password: password
 });
-svc.on('install', () => {
-  svc.start();
-});
-svc.install();
+
+if (svc.exists) {
+  svc.stop(() => {
+    svc.uninstall(() => {
+      installService();
+    });
+  });
+} else {
+  installService();
+}
+function installService() {
+  svc.on('install', () => {
+    svc.start();
+  });
+  svc.install();
+}
